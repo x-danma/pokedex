400a661ec1919764ea83d08d74093b8de5188cee
Daniel Mårtensson <daniel.martensson89@outlook.com>
2023-11-11 22:25:22 +0000

    some css

--- frontend/pokedex/src/PokemonCard.tsx
+++ frontend/pokedex/src/PokemonCard.tsx
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import { getTypeIcon } from './getTypeIcon';
 
 type PokemonCardProps = {
-  url: string;
+    url: string;
 };
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ url }) => {
-  const [pokemon, setPokemon] = useState<{ name: string; image: string; type: string } | null>(null);
+    const [pokemon, setPokemon] = useState<{ number: number; name: string; image: string; type: string[] } | null>(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          setPokemon({
-            name: data.name,
-            image: data.sprites.front_default,
-            type: data.types.map((typeInfo: { type: { name: any; }; }) => typeInfo.type.name).join(', ')
-          });
-        });
-    }, 1000);
-  }, [url]);
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    setPokemon({
+                        number: data.id, // Add this line
+                        name: data.name,
+                        image: data.sprites.front_default,
+                        type: data.types.map((typeInfo: { type: { name: any; }; }) => typeInfo.type.name)
+                    });
+                });
+        }, 1000);
+    }, [url]);
 
-  return (
-    <div className="card">
-      {pokemon ? (
-        <>
-          <h2>{pokemon.name}</h2>
-          <img src={pokemon.image} alt={pokemon.name} />
-          <p>Type: {pokemon.type}</p>
-        </>
-      ) : (
-        <div className="spinner"></div>
-      )}
-    </div>
-  );
+    return (
+        <div className="card">
+            {pokemon ? (
+                <>
+                    <h2> {pokemon.number}. {pokemon.name.toUpperCase()}</h2>
+                    <img src={pokemon.image} alt={pokemon.name} />
+                    <div className="type-icons">
+                        {pokemon.type.map((type, index) => (
+                            <img key={index} className="type-icon" src={getTypeIcon(type)} alt={type} />
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <div className="spinner"></div>
+            )}
+        </div>
+    );
 }
 
 export default PokemonCard;

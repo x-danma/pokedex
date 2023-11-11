6c324cb7fd36b3ad2a4c1b0906f80c38acedb22c
Daniel Mårtensson <daniel.martensson89@outlook.com>
2023-11-11 19:57:50 +0000

    refactor (easy with GH CoPilot)

--- frontend/pokedex/src/Pokemon.tsx
+++ frontend/pokedex/src/Pokemon.tsx
@@ -1,37 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import PokemonCard from './PokemonCard';
 
-export interface PokemonData {
-  name: string;
-  image: string;
-  type: string;
-}
-
 const Pokemon = () => {
-  const [pokemon, setPokemon] = useState<PokemonData[]>([]);
+  const [pokemonUrls, setPokemonUrls] = useState<string[]>([]);
 
   useEffect(() => {
-    Promise.all([
-      fetch('https://pokeapi.co/api/v2/pokemon/pikachu'),
-      fetch('https://pokeapi.co/api/v2/pokemon/arbok')
-    ])
-      .then((responses) => Promise.all(responses.map((response) => response.json())))
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=12')
+      .then((response) => response.json())
       .then((data) => {
-        const pokemonData = data.map((pokemon) => ({
-          name: pokemon.name,
-          image: pokemon.sprites.front_default,
-          type: pokemon.types.map((typeInfo: { type: { name: any; }; }) => typeInfo.type.name).join(', ')
-        }));
-        setPokemon(pokemonData);
+        const urls = data.results.map((pokemon: { url: string }) => pokemon.url);
+        setPokemonUrls(urls);
       });
   }, []);
 
-  if (pokemon.length === 0) return <div>Loading...</div>;
+  if (pokemonUrls.length === 0) return <div>Loading...</div>;
 
   return (
     <div className="pokemon-grid">
-    {pokemon.map((poke, index) => <PokemonCard key={index} pokemon={poke} />)}
-  </div>
+      {pokemonUrls.map((url, index) => <PokemonCard key={index} url={url} />)}
+    </div>
   );
 }
 
--- frontend/pokedex/src/PokemonCard.tsx
+++ frontend/pokedex/src/PokemonCard.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { PokemonData } from './Pokemon'; // Import the PokemonData type from the Pokemon file
+import React, { useState, useEffect } from 'react';
 
 type PokemonCardProps = {
-  pokemon: PokemonData;
+  url: string;
 };
 
-const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
+const PokemonCard: React.FC<PokemonCardProps> = ({ url }) => {
+  const [pokemon, setPokemon] = useState<{ name: string; image: string; type: string } | null>(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon({
+          name: data.name,
+          image: data.sprites.front_default,
+          type: data.types.map((typeInfo: { type: { name: any; }; }) => typeInfo.type.name).join(', ')
+        });
+      });
+  }, [url]);
+
+  if (!pokemon) return <div>Loading...</div>;
+
   return (
     <div className="card">
       <h2>{pokemon.name}</h2>

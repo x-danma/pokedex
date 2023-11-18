4aec3225ba2566056eac8bb0154cea6dc23c8d28
Daniel Mårtensson <daniel.martensson89@outlook.com>
2023-11-18 09:00:56 +0000

    try table switcj

--- frontend/pokedex/src/Pokemon.tsx
+++ frontend/pokedex/src/Pokemon.tsx
@@ -50,8 +50,7 @@ const Pokemon = () => {
   return (
     <>
       <div style={{ display: 'flex', position: 'fixed', top: 0, background: '#fff', padding: '10px', zIndex: 100 }}>
-        // mui button that does nothing
-        <Button>Button</Button>
+       
         
         <button style={{ marginRight: '20px' }} onClick={toggleShowFirstFive}>
           {showOnlyFirstFive ? 'Show All' : 'Show Only First Five'}
--- frontend/pokedex/src/PokemonList.tsx
+++ frontend/pokedex/src/PokemonList.tsx
@@ -1,42 +1,66 @@
 import { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Tab } from '@mui/material';
+import { Pokedex } from 'pokeapi-js-wrapper';
+import { getTypeIcon } from './getTypeIcon';
 
 type Pokemon = {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
+    type: string; // Add the type property
 };
 
+const P = new Pokedex();
+
 function PokemonList() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
 
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=5')
-      .then(response => response.json())
-      .then(data => setPokemon(data.results));
-  }, []);
+    useEffect(() => {
+        P.getPokemonsList({ limit: 6 })
+            .then((response: { results: any[]; }) => {
+                // For each Pokemon, fetch its details to get its type
+                const promises = response.results.map(poke =>
+                    P.getPokemonByName(poke.name).then(details => ({
+                        name: poke.name,
+                        url: poke.url,
+                        type: details.types[0].type.name, // Assume the first type is the main type
+                    }))
+                );
 
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Pokemon Name</TableCell>
-            <TableCell align="right">Pokemon URL</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {pokemon.map((poke: Pokemon) => (
-            <TableRow key={poke.name}>
-              <TableCell component="th" scope="row">
-                {poke.name}
-              </TableCell>
-              <TableCell align="right">{poke.url}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+                // Update the state once all promises have resolved
+                Promise.all(promises).then(setPokemon);
+            });
+    }, []);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Pokemon Name</TableCell>
+                        <TableCell align="right">Pokemon URL</TableCell>
+                        <TableCell align="right">Pokemon Type</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {pokemon.map((poke: Pokemon) => (
+                        <TableRow key={poke.name}>
+                            <TableCell component="th" scope="row">
+                                {poke.name}
+                            </TableCell>
+                            <TableCell align="right">{poke.url}</TableCell>
+                            <TableCell align="right">{poke.type}</TableCell>
+
+                            <TableCell align="right">
+                                {poke.type.split(',').map((type, index) => (
+                                    <img key={index} src={getTypeIcon(type.trim())} alt={type.trim()} />
+                                ))}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
 
 export default PokemonList;

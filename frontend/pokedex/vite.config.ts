c3d1dd9888a94ed6d155ce18092b0394e09e85fe
Daniel Mårtensson <daniel.martensson89@outlook.com>
2023-11-18 08:34:04 +0000

    working mui

--- frontend/pokedex/vite.config.ts
+++ frontend/pokedex/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '/node_modules/': path.resolve('pokedex', './node_modules/'),
+    },
+    dedupe: ['react', 'react-dom'] // Add other packages as needed
+  }
 })

66f405749dbf70e9e3c5d930f66f8f7364046361
Daniel Mårtensson <95696986+daniel-martensson@users.noreply.github.com>
2023-12-31 21:34:08 +0000

    Update PokemonList component to render PokemonCard components in chunks

--- frontend/pokedex/src/PokemonList.tsx
+++ frontend/pokedex/src/PokemonList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import PokemonCard from './PokemonCard';
 
 type Pokemon = {
   name: string;
@@ -10,7 +11,7 @@ function PokemonList() {
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=5')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
       .then(response => response.json())
       .then(data => setPokemon(data.results));
   }, []);
@@ -18,19 +19,14 @@ function PokemonList() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Pokemon Name</TableCell>
-            <TableCell align="right">Pokemon URL</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
-          {pokemon.map((poke: Pokemon) => (
-            <TableRow key={poke.name}>
-              <TableCell component="th" scope="row">
-                {poke.name}
-              </TableCell>
-              <TableCell align="right">{poke.url}</TableCell>
+          {chunkArray(pokemon, 5).map((chunk, chunkIndex) => (
+            <TableRow key={chunkIndex}>
+              {chunk.map((poke: Pokemon) => (
+                <TableCell key={poke.name}>
+                  <PokemonCard url={poke.url} isShiny={false} /> {/* Render the PokemonCard component */}
+                </TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
@@ -40,3 +36,11 @@ function PokemonList() {
 }
 
 export default PokemonList;
+
+function chunkArray(array: any[], chunkSize: number) {
+  const results = [];
+  while (array.length) {
+    results.push(array.splice(0, chunkSize));
+  }
+  return results;
+}

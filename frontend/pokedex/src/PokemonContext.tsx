98d1318a21109364a26724fdbbbf11b80bce4dcc
Daniel Mårtensson <95696986+daniel-martensson@users.noreply.github.com>
2023-12-31 21:25:20 +0000

    Refactor PokemonContext and add TypeScript interface

--- frontend/pokedex/src/PokemonContext.tsx
+++ frontend/pokedex/src/PokemonContext.tsx
@@ -3,41 +3,46 @@ import React, { createContext, useState, useEffect, useContext } from 'react';
 import { Pokedex } from 'pokeapi-js-wrapper';
 
 const options = {
-  protocol: 'https',
-  versionPath: '/api/v2/',
-  cache: true,
-  timeout: 5 * 1000 // 5s
+    protocol: 'https',
+    versionPath: '/api/v2/',
+    cache: true,
+    timeout: 5 * 1000 // 5s
 }
 const P = new Pokedex(options);
 
-const PokemonContext = createContext(null);
+interface PokemonContextValue {
+    pokemonUrls: string[];
+    fetchPokemon: (url: string) => void;
+}
 
-export const PokemonProvider = ({ children }) => {
-  const [pokemonUrls, setPokemonUrls] = useState<string[]>([]);
+const PokemonContext = createContext<PokemonContextValue | null>(null);
 
-  const fetchPokemon = (url: string) => {
-    P.resource(url)
-      .then((response: { results: { url: string; }[]; next: string; }) => {
-        const urls = response.results.map((pokemon: { url: string }) => pokemon.url);
-        setPokemonUrls(prevUrls => [...prevUrls, ...urls]);
-      });
-  };
+export const PokemonProvider = ({ children }: { children: React.ReactNode }) => {
+    const [pokemonUrls, setPokemonUrls] = useState<string[]>([]);
 
-  useEffect(() => {
-    fetchPokemon('/api/v2/pokemon?limit=1010');
-  }, []);
+    const fetchPokemon = (url: string) => {
+        P.resource(url)
+            .then((response: { results: { url: string; }[]; next: string; }) => {
+                const urls = response.results.map((pokemon: { url: string }) => pokemon.url);
+                setPokemonUrls(prevUrls => [...prevUrls, ...urls]);
+            });
+    };
 
-  return (
-    <PokemonContext.Provider value={{ pokemonUrls, fetchPokemon }}>
-      {children}
-    </PokemonContext.Provider>
-  );
+    useEffect(() => {
+        fetchPokemon('/api/v2/pokemon?limit=100');
+    }, []);
+
+    return (
+        <PokemonContext.Provider value={{ pokemonUrls, fetchPokemon }}>
+            {children}
+        </PokemonContext.Provider>
+    );
 };
 
 export const usePokemon = () => {
-  const context = useContext(PokemonContext);
-  if (context === null) {
-    throw new Error('usePokemon must be used within a PokemonProvider');
-  }
-  return context;
+    const context = useContext(PokemonContext);
+    if (context === null) {
+        throw new Error('usePokemon must be used within a PokemonProvider');
+    }
+    return context;
 };

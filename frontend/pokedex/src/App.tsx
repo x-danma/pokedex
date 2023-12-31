3d80842cf7f63639654c3906fc25b7954d3b9640
Daniel Mårtensson <95696986+daniel-martensson@users.noreply.github.com>
2023-12-31 20:56:46 +0000

    add mui tabs for multiple components

--- frontend/pokedex/src/App.tsx
+++ frontend/pokedex/src/App.tsx
@@ -1,12 +1,48 @@
-import './App.css'
-// import Pokemon from './Pokemon'
+import React, { useState } from 'react';
+import './App.css';
+// import Pokemon from './Pokemon';
 import PokemonList from './PokemonList';
+import { Tabs, Tab, Box } from '@mui/material';
+
+function TabPanel(props: any) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <>
-      <PokemonList />
-     {/* <Pokemon /> */}
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Pokemon List" />
+        <Tab label="Pokemon" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <PokemonList />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        {/* <Pokemon /> */}
+      </TabPanel>
     </>
   );
 }

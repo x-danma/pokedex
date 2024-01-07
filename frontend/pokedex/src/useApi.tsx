62b371658d0d39a64c2b4318ebd32a095e87e44c
Daniel Mårtensson <95696986+daniel-martensson@users.noreply.github.com>
2024-01-07 20:02:24 +0000

    fix for github codespaces

--- frontend/pokedex/src/useApi.tsx
+++ frontend/pokedex/src/useApi.tsx
@@ -1,5 +1,20 @@
+
+function getApiBaseUrl() {
+  const apiBaseUrl = 'http://localhost:5025';
+  const hostname = window.location.hostname;
+  const apiPort = '5025';
+  const clientPort = '5173';
+  if (hostname.includes('github.dev')) {
+    const newApiBaseUrl = hostname.replace(clientPort, apiPort);
+    return `https://${newApiBaseUrl}`;
+  }
+  return apiBaseUrl;
+}
+
 const useApi = () => {
-  const baseUrl = "http://localhost:5025";
+  // get baseurl from environment file
+
+  const baseUrl = getApiBaseUrl();
 
   return {
     HealthCheck: () => fetch(`${baseUrl}/healthy`),
